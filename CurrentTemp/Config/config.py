0f91673c9314cde31a5562f206c6efad76e1266b
anksrv96 <103916918+anksrv96@users.noreply.github.com>
2022-04-25 00:35:47 +0530

    Add files via upload

---
+++ CurrentTemp/Config/config.py
@@ -0,0 +1,6 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "/Users/ankshrivastav/PycharmProjects/CurrentTemp/drivers/chromedriver"
+
+    BASE_URL = "https://weathershopper.pythonanywhere.com/"
+
+    HOMEPAGE_TITLE = 'Current Temperature'
